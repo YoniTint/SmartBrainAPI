94875ae39ca1a022f5135576b1b3ca2d2f735f7d
YoniTint <yonitint1@gmail.com>
2024-04-09 14:16:38 +0300

    added profile update

--- controllers/profile.js
+++ controllers/profile.js
@@ -12,6 +12,23 @@ handleProfileGet = (req, res, db) => {
         });
 };
 
+const handleProfileUpdate = (req, res, db) => {
+    const { id } = req.params;
+    const { name, age, pet } = req.body.formInput;
+    db('users')
+        .where({ id })
+        .update({ name })
+        .then(resp => {
+          if (resp) {
+              res.json("success");
+          } else {
+              res.status(400).json('Unable to update')
+          }
+        })
+        .catch(err => res.status(400).json('error updating user'))
+}
+
 module.exports = {
     handleProfileGet,
+    handleProfileUpdate
 };
